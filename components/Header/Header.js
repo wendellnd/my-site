7372aca0644d73b20efa368660fa633d25e4140d
wendellnd <wendelldourado.wn@gmail.com>
2021-02-27 17:51:41 -0300

    disrupting Header props

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,12 @@
 import styles from './styles.module.css'
 
-const Header = (props) => {
+const Header = ({options} = props) => {
   return (
     <header className={styles.header}>
       <div className={styles.menu}>WendellNascimentoDourado</div>
       <div>
         <ul className={styles.list}>
-          {props.options.map((option, index) => (
+          {options.map((option, index) => (
             <li key={index}>
               <a href={option.link}>{option.label}</a>
             </li>
