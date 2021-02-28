7a9264d4ab0191426f73e309a452089c2fd27798
wendellnd <wendelldourado.wn@gmail.com>
2021-02-28 02:45:16 -0300

    fix remove home styles

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import Header from '../components/Header/Header'
 import Speech from '../components/Speech/Speech'
 import * as utils from '../utils/constants'
-import styles from '../styles/home.module.css'
-
 const Home = () => {
   return (
-    <div className={styles.body}>
+    <>
       <Header options={utils.options} />
 
       <Speech />
-    </div>
+    </>
   )
 }
 
