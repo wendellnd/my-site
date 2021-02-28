2b212e08bdbba7c1df69988b1e0bc8f4e6ddaf70
wendellnd <wendelldourado.wn@gmail.com>
2021-02-28 01:10:15 -0300

    detaching Speech as a component

---
+++ components/Speech/Speech.js
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import { useState } from 'react'
+import styles from './styles.module.css'
+
+const Speech = () => {
+    const [speechIndex, setSpeechIndex] = useState(0)
+    
+    const speechs = [
+      'Hello! if you’re here, I think you wanna know about me',
+      'Fortunately,  I will share it with you ;)',
+      'So, click here and let’s start!',
+    ]
+  
+    const changeSpeech = () => {
+      if (speechIndex < 2) {
+        setSpeechIndex(speechIndex + 1)
+      }
+    }
+    return (
+        <div className={styles.speechAlign}>
+        <Image
+          src="/speech-balloon_1.png"
+          width={400}
+          height={500}
+          className={styles.me}
+        />
+        <div className={styles.speechBalloon}>
+          <button onClick={changeSpeech}>
+            <p>{speechs[speechIndex]}</p>
+          </button>
+        </div>
+      </div>
+    )
+}
+
+export default Speech
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,15 @@
 import Header from '../components/Header/Header'
-import Image from 'next/image'
-import { useState } from 'react'
+import Speech from '../components/Speech/Speech'
 import * as utils from '../utils/constants'
 import styles from '../styles/home.module.css'
 
 const Home = () => {
-  const [speechIndex, setSpeechIndex] = useState(0)
-  const speechs = [
-    'Hello! if you’re here, I think you wanna know about me',
-    'Fortunately,  I will share it with you ;)',
-    'So, click here and let’s start!',
-  ]
-
-  const changeSpeech = () => {
-    if (speechIndex < 2) {
-      setSpeechIndex(speechIndex + 1)
-    }
-  }
 
   return (
     <body className={styles.body}>
       <Header options={utils.options} />
 
-      <div className={styles.speechAlign}>
-        <Image
-          src="/speech-balloon_1.png"
-          width={400}
-          height={500}
-          className={styles.me}
-        />
-        <div className={styles.speechBalloon}>
-          <button onClick={changeSpeech}>
-            <p>{speechs[speechIndex]}</p>
-          </button>
-        </div>
-      </div>
+      <Speech />
     </body>
   )
 }
