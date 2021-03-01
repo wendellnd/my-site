6601181b8ed349c8f5556b92043ac02c5b4acc67
wendellnd <wendelldourado.wn@gmail.com>
2021-03-01 01:18:59 -0300

    alter import

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Header from '../components/Header/Header';
 import Speech from '../components/Speech/Speech';
-import * as utils from '../utils/constants';
+import { options } from '../utils/constants';
 const Home = () => {
   return (
     <>
-      <Header options={utils.options} />
+      <Header options={options} />
 
       <Speech />
     </>
