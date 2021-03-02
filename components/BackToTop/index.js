7877f25a60711b92f4f68632c18b8347a287cb3a
wendellnd <wendelldourado.wn@gmail.com>
2021-03-02 00:24:35 -0300

    backToTop only appears when scroll

--- components/BackToTop/index.js
+++ components/BackToTop/index.js
@@ -1,11 +1,24 @@
-import styles from './styles.module.css';
+import { useWindowScroll } from 'react-use';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import styles from './styles.module.css';
 
 const BackToTop = () => {
+  const { y: pageYOffset } = useWindowScroll();
+
+  const [visible, setVisibility] = useState(false);
+
+  useEffect(() => {
+    pageYOffset >= 250 ? setVisibility(true) : setVisibility(false);
+  }, [pageYOffset]);
+
   const scrollToTop = () => window.scrollTo({ top: 0, behavior: 'smooth' });
 
   return (
-    <button className={styles.backToTop} onClick={scrollToTop}>
+    <button
+      className={`${styles.backToTop} ${!visible ? styles.visibility : ''}`}
+      onClick={scrollToTop}
+    >
       <Image src="/arrow_up.svg" width={20} height={25} />
     </button>
   );
