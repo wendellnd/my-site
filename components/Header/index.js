a5e164d03a95a59c810e7fac7f9e9c6dd8e7410f
WendellNDRV <wnascimento@redventures.com>
2021-05-06 15:37:57 -0300

    refac header

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,27 +3,24 @@ import Link from 'next/link';
 
 const Header = ({ options }) => {
   return (
-    <header className={styles.headerBody}>
-      <div className={styles.header}>
-        <Link href="/">
-          <a className={styles.backToMenu}>
-            WendellN<div className={styles.nas}>ascimento</div>D
-            <div className={styles.dou}>ourado</div>
-          </a>
-        </Link>
-        <div>
-          <ul className={styles.list}>
-            {options &&
-              options.map((option, index) => (
-                <li key={index}>
-                  <Link href={option.link}>
-                    <a>{option.label}</a>
-                  </Link>
-                </li>
-              ))}
-          </ul>
-        </div>
-      </div>
+    <header className={styles.header}>
+      <Link href="/">
+        <a className={styles.backToMenu}>
+          WendellN<div className={styles.nas}>ascimento</div>D
+          <div className={styles.dou}>ourado</div>
+        </a>
+      </Link>
+
+      <ul className={styles.list}>
+        {options &&
+          options.map((option, index) => (
+            <li key={index}>
+              <Link href={option.link}>
+                <a>{option.label}</a>
+              </Link>
+            </li>
+          ))}
+      </ul>
     </header>
   );
 };
